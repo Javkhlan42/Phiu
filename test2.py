7e9307538538d08b45dfb38e38c0b27908a02446
Javkhlan42 <147017349+Javkhlan42@users.noreply.github.com>
2024-05-21 15:15:51 +0800

    hello

--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
 map_of_values = {'name': 'Andy', 'surname': 'Bieber', 'marital_status': 'married', 'country': 'Great Britain'}
 for key, value in map_of_values.items():
-    print(key + ' ' + value)
+    print(key + ' ' + value); 
+print("Hello world ")
